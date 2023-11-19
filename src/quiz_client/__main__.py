b338487d0b76da5994dcbe65759cbf008802c385
Martin Zeleny <mzeleny@redhat.com>
2023-11-19 21:43:08 +0100

    Day 3

--- src/quiz_client/__main__.py
+++ src/quiz_client/__main__.py
@@ -1,18 +1,20 @@
 import asyncio
 from websockets import connect, WebSocketClientProtocol
+from websockets.exceptions import ConnectionClosedError
 import aioconsole
 import sys
 
 async def send_receive_messages(uri: str, client_id: str):
     async with connect(uri) as ws:
-        send_task = asyncio.create_task(send_messages(ws, client_id))
-        receive_task = asyncio.create_task(receive_messages(ws))
-        await asyncio.gather(send_task, receive_task)
+        await asyncio.gather(
+            asyncio.create_task(send_messages(ws, client_id)),
+            asyncio.create_task(receive_messages(ws))
+        )
 
 
 async def send_messages(ws: WebSocketClientProtocol, client_id: str):
     while True:
-        user_input = await aioconsole.ainput(f"Client {client_id}: ")
+        user_input = await aioconsole.ainput(f"\nClient {client_id}: ")
         if not user_input:
             continue
         await ws.send(f"{client_id}: {user_input}")
@@ -21,7 +23,7 @@ async def send_messages(ws: WebSocketClientProtocol, client_id: str):
 async def receive_messages(ws: WebSocketClientProtocol):
     while True:
         response = await ws.recv()
-        print(f"\nReceived from server: {response}")
+        print(f"{response}")
 
 
 def main():
@@ -30,9 +32,17 @@ def main():
         sys.exit(1)
 
     client_id = sys.argv[1]
-    server_uri = f"ws://127.0.0.1:8000/ws/{client_id}"
+    server_uri = f"ws://127.0.0.1:8000/register/{client_id}"
 
-    asyncio.get_event_loop().run_until_complete(send_receive_messages(server_uri, client_id))
+    print("Press Ctrl+C to exit")
+    try:
+        asyncio.get_event_loop().run_until_complete(send_receive_messages(server_uri, client_id))
+    except ConnectionClosedError:
+        print("\nConnection closed")
+        sys.exit()
+    except KeyboardInterrupt:
+        print("\nExit client")
+        sys.exit()
 
 
 if __name__ == "__main__":
